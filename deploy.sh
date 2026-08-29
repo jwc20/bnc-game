17b6d24785f1be7995a21d7fb676b12063f1c581
jw <choi.jae100@gmail.com>
2026-08-29 07:50:42 -0700

    quick fix

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "Deploying $DOMAIN..."
 git pull origin main
 
 echo "Updating Git submodules..."
-git config url.https://github.com/.insteadOf git@github.com:
+git config --global url.https://github.com/.insteadOf git@github.com:
 git submodule sync --recursive
 git submodule update --init --recursive --remote
 
